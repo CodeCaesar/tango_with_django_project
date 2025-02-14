ec87fc541218adf89edcff0b0b3f1f3e9e1d39f3
Adrian Czerwinski <2842259c@student.gla.ac.uk>
2025-02-14 12:10:42 +0000

    Fixed populate rango after recreating 0001_initial, now only test_page_display_changes fails

--- populate_rango.py
+++ populate_rango.py
@@ -33,7 +33,7 @@ def populate():
             'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
     
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
     
@@ -42,6 +42,7 @@ def populate():
             print(f'- {c}: {p}')
 
 def add_page(cat, title, url, views=0):
+    print(f"Adding page {title}")
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url=url
     p.views=views
@@ -49,6 +50,7 @@ def add_page(cat, title, url, views=0):
     return p
 
 def add_cat(name, views=0, likes=0):
+    print(f"Adding category {name}")
     c = Category.objects.get_or_create(name=name)[0]#, views=views, likes=likes)[0]
     c.views=views
     c.likes=likes
--- rango/models.py
+++ rango/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    #views = models.IntegerField(default=0)
-    #likes = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
     
     # Makes "Categorys" be "Categories"
     class Meta:
